a4b71b34329ca8199ec07f9d8febb292a9ba9c71
Tomoyuki Tanaka <tanaka-tomoyuki-wy@ynu.jp>
2022-05-24 13:44:18 +0900

    WIP

---
+++ energy/maj3_energy/foo.py
@@ -0,0 +1,25 @@
+
+
+item = [
+    "XI3","XI5",
+    "XI0.XI6",
+    "XI7","XI17","XI8","XI18"]
+for i in item:
+    print(f".print v(7.{i})")
+    print(f".print v(8.{i})")
+    print()
+
+
+for i in range (14):
+    print(f"{i+1} {i+1} *")
+
+# ind1 = ["-3","-2","-1"]
+# ind2 = ["a","b","c","d","e"]
+# ind3 = ["l","r"]
+
+# for i2 in ind2:
+#     for i3 in ind3:
+#         print(f"\"maj_{i2}_{i3}\",",end ="")
+
+
+print()
